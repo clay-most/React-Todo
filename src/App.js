8832bf66366537ef6dfaa22831b8b4d384fbe51e
clay-most <46730958+clay-most@users.noreply.github.com>
2019-06-06 19:51:27 -0600

    basic app (obvious bugs)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React from "react";
+import TodoList from "./components/TodoComponents/TodoList";
+import TodoForm from "./components/TodoComponents/TodoForm";
+
+import "./components/TodoComponents/Todo.css"
+
+const todos = [
+  { task: "build app", id: 1, completed: false },
+  { task: "style app", id: 2, completed: false }
+];
 
 class App extends React.Component {
-  constuctor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      tasks: []
+      todos
     };
   }
+
+  addTodo = todo => {
+    const newTodo = { task: todo, id: Date.now(), completed: false };
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  };
+
+  clearCompleted = () => {
+    const completedTodos = this.state.todos.filter(
+      todo => todo.completed === false
+    );
+    this.setState({ todos: completedTodos });
+  };
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1>Welcome to your Todo App!</h1>
+        <TodoList todos={this.state.todos} toggleTodo={this.toggleTodo} />
+        <TodoForm addTodo={this.addTodo} clearCompleted={this.clearCompleted} />
       </div>
     );
   }
