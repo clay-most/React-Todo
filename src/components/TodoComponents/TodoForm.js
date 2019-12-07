f70aedb454eadab2c670a0f99f3ded87e7ccfe10
Anna Morris <annapatton444@gmail.com>
2019-12-07 11:46:05 -0600

    Update TodoForm.js

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,11 +1,41 @@
 import React from "react";
 
-function TodoForm(props) {
-  return (
-    <div>
-      <form>I am a form</form>
-    </div>
-  );
+class TodoForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newThing: ""
+    };
+
+    
+  }
+  
+    changeHandler = event => {
+      this.setState({ newThing: event.target.value });
+    };
+
+    submitHandler = event => {
+      event.preventDefault();
+      if (this.state.newThing !== "") {
+        this.props.addTask(this.state.newThing);
+        this.setState({ newThing: "" });
+      }
+    };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.submitHandler}>
+          <input
+            name="newThing"
+            value={this.state.newThing}
+            onChange={this.changeHandler}
+          ></input>
+          <button type="submit">Create</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default TodoForm;
